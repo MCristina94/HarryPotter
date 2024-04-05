8c0df8f170c47b2bfa290842e44d2e0f70e3ff1e
MCristina94 <cmaureen1908@gmail.com>
2024-04-05 12:01:42 -0500

    se agregan iconos a la card y se cambian logos

--- HarryPotter/src/Components/Card.jsx
+++ HarryPotter/src/Components/Card.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import c from './css/card.module.css'
-import logoGryffindor from '../img/gryffindor.png'
-import logoSlytherin from '../img/stlitherin.png'
-import logoHufflepuff from '../img/hyfflepuff.png'
-import logoRavenclaw from '../img/ravenclaw.png'
-import logoHogwarts from '../img/Logo-Hogwarts.png'
+import React from "react";
+import c from "./css/card.module.css";
+import logoGryffindor from "../img/Gryffindor.png";
+import logoSlytherin from "../img/Slytherin.png";
+import logoHufflepuff from "../img/Hufflepuff.png";
+import logoRavenclaw from "../img/Ravenclaw.png";
+import logoHogwarts from "../img/houses.png";
 
-const Card = ({student}) => {
+const Card = ({ student }) => {
   let imageStudent;
-  if(student.image && student.image !== ''){
+  if (student.image && student.image !== "") {
     imageStudent = student.image;
-  }else{
+  } else {
     // Si student.image está vacío o no existe, entonces verifica la casa y asigna la imagen correspondiente
-    if(student.house === "Gryffindor"){
+    if (student.house === "Gryffindor") {
       imageStudent = logoGryffindor;
-    }else if(student.house === "Slytherin"){
+    } else if (student.house === "Slytherin") {
       imageStudent = logoSlytherin;
-    }else if(student.house === "Hufflepuff"){
+    } else if (student.house === "Hufflepuff") {
       imageStudent = logoHufflepuff;
-    }else if(student.house === "Ravenclaw"){
+    } else if (student.house === "Ravenclaw") {
       imageStudent = logoRavenclaw;
-    }else{
+    } else {
       // Si no se encuentra ninguna casa, asigna el logo de Hogwarts
       imageStudent = logoHogwarts;
     }
@@ -28,17 +28,21 @@ const Card = ({student}) => {
 
   console.log(student.house);
   return (
-    
-        <div className={c.card}>
-                <img src={imageStudent} alt='photoCharacter' className={c.img}/>  
-            <div className={c.info}>
-                <h3>{student.name}</h3>
-                <p>Casa: {student.house}</p> 
-            </div>
-             
-        </div>
-        
-  )
-}
+    <div className={`${c.card} ${c[student.house.toLowerCase()]}`}>
+      <img src={imageStudent} alt="photoCharacter" className={c.img} />
+      <div className={c.info}>
+        <h3>{student.name}</h3>
+        <p>Casa: {student.house}</p>
+      </div>
+      <div style={{ position: 'absolute', top: 20, right: 50 }}>
+        <i class="fa-solid fa-heart" ></i>
+      </div>
+      <div style={{ position: 'absolute', top: 180, right: 50}}>
+      <i class="fa-solid fa-up-right-from-square" ></i> 
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
+
